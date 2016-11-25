0fc765bf1326c52fce41cac769449d2741636754
Marko Vukobrat <marko.vukobrat@sbgenomics.com>
2016-11-25 13:05:07 +0100

    debuging paths

--- rabix-integration-testing/cwlstarter/draft-2_starter.sh
+++ rabix-integration-testing/cwlstarter/draft-2_starter.sh
@@ -2,8 +2,10 @@
 virtualenv env/testenv
 source env/testenv/bin/activate
 pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
-echo "nesto_rabix"
+echo "deb1.trenutno u pathu:"
+pwd
+echo "deb2.nesto_rabix"
 ls -l /home/travis/build/rabix/bunny/rabix-backend-local/target/
-echo "nesto_conf"
-ls -l conformance_test_draft-2.yaml
+echo "deb3.ls nad trenutnim pathom"
+ls -l
 cwltest --test conformance_test_draft-2.yaml --tool rabix -j 4 > /home/travis/build/markosbg/debug/rabix-integration-testing/common-workflow-language/draft-2/resultConf.txt
