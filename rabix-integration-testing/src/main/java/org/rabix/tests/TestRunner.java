8c429c3734f73d9fa6a95565332d46c1ccd8ca85
Marko Vukobrat <marko.vukobrat@sbgenomics.com>
2016-11-25 11:36:03 +0100

    no more inline + writing in file

--- rabix-integration-testing/src/main/java/org/rabix/tests/TestRunner.java
+++ rabix-integration-testing/src/main/java/org/rabix/tests/TestRunner.java
@@ -72,7 +72,7 @@ public class TestRunner {
 		
 		//TODO END DEBUG
 		
-		executeConformanceSuite("#!/bin/bash | virtualenv env/testenv | source env/testenv/bin/activate | pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest | cwltest --test conformance_test_draft-2.yaml --tool /home/travis/build/rabix/bunny/rabix-backend-local/target/rabix -j 4", cwlTestWorkingdir);
+		executeConformanceSuite("./" + starterScriptName, cwlTestWorkingdir);
 		logger.info("Conformance test ended: " + draftName);
 		
 	}
