85dab99d47cccb45e33f62621537e063e77ae013
Marko Vukobrat <marko.vukobrat@sbgenomics.com>
2016-11-25 13:34:10 +0100

    without parallel -j 4

--- rabix-integration-testing/cwlstarter/draft-2_starter.sh
+++ rabix-integration-testing/cwlstarter/draft-2_starter.sh
@@ -2,10 +2,4 @@
 virtualenv env/testenv
 source env/testenv/bin/activate
 pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
-echo "deb1.trenutno u pathu:"
-pwd
-echo "deb2.nesto_rabix"
-ls -l /home/travis/build/markosbg/debug/rabix-backend-local/target/
-echo "deb3.ls nad trenutnim pathom"
-ls -l
-cwltest --test conformance_test_draft-2.yaml --tool /home/travis/build/markosbg/debug/rabix-backend-local/target/rabix -j 4
+cwltest --test conformance_test_draft-2.yaml --tool /home/travis/build/markosbg/debug/rabix-backend-local/target/rabix
