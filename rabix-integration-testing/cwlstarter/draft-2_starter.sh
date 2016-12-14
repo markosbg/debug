21250f2df2bc80e12bb9e4ad8515473692a12fb0
Marko Vukobrat <marko.vukobrat@sbgenomics.com>
2016-12-14 18:01:25 +0100

    Updpdp

--- rabix-integration-testing/cwlstarter/draft-2_starter.sh
+++ rabix-integration-testing/cwlstarter/draft-2_starter.sh
@@ -4,4 +4,6 @@ source env/testenv/bin/activate
 pip install -e git+https://github.com/common-workflow-language/cwltest.git@master#egg=cwltest
 echo komanda
 echo cwltest --test conformance_test_draft-2.yaml --tool ${buildFileDirPath}rabix -j 4
-cwltest --test conformance_test_draft-2.yaml --tool ${buildFileDirPath}rabix -j 4
+cwltest --test conformance_test_draft-2.yaml --tool ${buildFileDirPath}rabix -j 4 > test.txt
+echo macka
+cat test.txt
